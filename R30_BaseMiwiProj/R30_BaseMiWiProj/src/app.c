e9ecf18e75bb6189dd31f058633f58a51d77bd9c
Brian Tate <brian.tate@microchip.com>
2018-10-25 15:51:02 -0500

    add ability to send broadcast data

--- R30_BaseMiwiProj/R30_BaseMiWiProj/src/app.c
+++ R30_BaseMiwiProj/R30_BaseMiWiProj/src/app.c
@@ -9,16 +9,36 @@
 #include "app.h"
 #include "debug_interface.h"
 
+#define SEND_BUFFER_SIZE  1
+
+static bool switchLastState = false;
+static bool switchState = false;
+static uint8_t cntVal = 0;
+static uint8_t sendDataBuffer[SEND_BUFFER_SIZE];
+uint8_t msghandledemo = 0;
+
+static void dataConfcb(uint8_t handle, miwi_status_t status, uint8_t* msgPointer)
+{
+	DEBUG_PRINT(printf("\nData Confirm: Handle: %d handle:%d\r\n", handle, status));
+	//MiMem_Free(msgPointer);
+}
+
 void AppTask(void)
 {
-	if(port_pin_get_input_level(SW0))
+	switchState = port_pin_get_input_level(SW0);
+	if(!switchState & switchLastState)
 	{
-		port_pin_set_output_level(LED1, LED_OFF);
+		port_pin_toggle_output_level(LED1);
+		sendDataBuffer[0] = 0x41 + cntVal%10; //start at ascii A
+		cntVal++;
+		DEBUG_PRINT(printf("sending char: %u\r\n", sendDataBuffer[0]));
+		
+		//send broadcast data
+		uint16_t broadcastAddress = 0xFFFF;
+		MiApp_SendData(SHORT_ADDR_LEN, (uint8_t *)&broadcastAddress,
+				SEND_BUFFER_SIZE, sendDataBuffer, msghandledemo++, true, dataConfcb);
 	}
-	else
-	{
-		port_pin_set_output_level(LED1,LED_ON);
-	}
+	switchLastState = switchState;
 }
 
 void ReceivedDataIndication (RECEIVED_MESSAGE *ind)
@@ -35,6 +55,8 @@ void ReceivedDataIndication (RECEIVED_MESSAGE *ind)
 	
 	for(uint8_t i=startPayloadIndex; i<rxMessage.PayloadSize;i++)
 	{
-		putchar(rxMessage.Payload[i]);
+		DEBUG_PRINT(putchar(rxMessage.Payload[i]));
 	}
+	DEBUG_PRINT(printf("\r\n"));
+	
 }
